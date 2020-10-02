7e149e796122878c9f734856fec3c3b149453448
Andre Eli <andre.eli@khk.ee>
2020-10-02 22:28:53 +0300

    Ülesanne 37.

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,11 @@
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        prindiTekst();
+    }
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Sisesta numbreid (-1 katkestab programmi töö)");
-
-        int summa = 0;
-        int loendur = 0;
-        int paaris = 0;
-        int paaritu = 0;
-
-        while (true) {
-            int number = Integer.parseInt(scanner.nextLine());
-            if (number == -1) {
-                break;
-            }
-            summa += number;
-            loendur++;
-
-            if (number % 2 == 0) {
-                paaris++;
-            }
-            else paaritu++;
-        }
-
-        double keskmine = (double) summa / loendur;
-
-        System.out.println("Kohtume hiljem!");
-        System.out.println("Summa on " + summa);
-        System.out.println("Sisestati " + loendur + " numbrit.");
-        System.out.println("Keskmine on " + keskmine);
-        System.out.println("Paaris numbreid on " + paaris);
-        System.out.println("Paarituid numbreid on " + paaritu);
+    public static void prindiTekst() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
     }
 }

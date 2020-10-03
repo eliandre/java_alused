c50b3624f191122b3c50581f631400740bc3ad19
Andre Eli <andre.eli@khk.ee>
2020-10-03 21:45:24 +0300

    Ülesanne 39.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,49 @@
 public class Main {
 
     public static void main(String[] args) {
-        prindiTekst();
+        printStars(3);
+        printStars(5);
+        printStars(9);
+        System.out.println();
+        printSquare(6);
+        System.out.println();
+        printRectangle(17, 3);
+        System.out.println();
+        printTriangle(4);
     }
 
-    public static void prindiTekst() {
-        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    public static void printStars(int kogus) {
+        int i = 0;
+        while (i < kogus) {
+            System.out.print("*");
+            i++;
+        }
+        System.out.println();
+    }
+
+    public static void printSquare(int kyljePikkus) {
+        int i = 0;
+        while (i < kyljePikkus) {
+            printStars(kyljePikkus);
+            i++;
+        }
+    }
+
+    public static void printRectangle(int laius, int korgus) {
+        int i = 0;
+        while (i < korgus) {
+            printStars(laius);
+            i++;
+        }
+    }
+
+    public static void printTriangle(int suurus) {
+        int i = 0;
+        int y = 1;
+        while (i < suurus) {
+            printStars(y);
+            i++;
+            y++;
+        }
     }
 }

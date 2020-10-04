72c31ddccba2d1561b5bdd65e40ead3644da4b0c
Andre Eli <andre.eli@khk.ee>
2020-10-04 21:12:43 +0300

    Ülesanne 5.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Andre Eli");
+
+        int paevad = 365;
+        int tunnid = 24;
+        int minutid = 60;
+        int sekundid = 60;
+        int sekundidAastas = paevad*tunnid*minutid*sekundid;
+
+        System.out.println("Aastas on " + sekundidAastas + " sekundit.");
     }
 
 }

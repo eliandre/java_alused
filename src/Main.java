69bea1921b55d9781df4176d5e08a019f762718b
Andre Eli <andre.eli@khk.ee>
2020-10-04 21:10:10 +0300

    Ülesanne 1.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-
-        double vastus = average(4, 3, 6, 1);
-        System.out.println("Arvude keskmine on " + vastus);
-
-    }
-
-    public static int summa(int a, int b, int c, int d){
-        return a + b + c + d;
-    }
-
-    public static double average(int number1, int number2, int number3, int number4){
-        int kokku = summa(number1, number2, number3, number4);
-        return (double) kokku / 4;
+        System.out.println("Andre Eli");
     }
 
 }

f037e37d49ddfa5b7a0e0df75c6bed0b1a584330
Andre Eli <andre.eli@khk.ee>
2020-10-04 20:58:58 +0300

    Ülesanne 46.

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int vastus = least(10, 7);
-        System.out.println("Väiksem on " + vastus);
+        double vastus = average(4, 3, 6, 1);
+        System.out.println("Arvude keskmine on " + vastus);
+
     }
 
-    public static int least(int number1, int number2) {
-        if (number1 > number2) {
-            return number2;
-        }
-        else if (number1 < number2) {
-            return number1;
-        }
-        else return number1;
+    public static int summa(int a, int b, int c, int d){
+        return a + b + c + d;
+    }
+
+    public static double average(int number1, int number2, int number3, int number4){
+        int kokku = summa(number1, number2, number3, number4);
+        return (double) kokku / 4;
     }
 
 }

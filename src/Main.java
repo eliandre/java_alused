30842caf555731f3e411a23f79b402b7cd1790f8
Andre Eli <andre.eli@khk.ee>
2020-10-04 21:28:16 +0300

    Ülesanne 21.

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,23 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Sisesta oma vanus: ");
-        int vanus = Integer.parseInt(scanner.nextLine());
+        System.out.print("Sisesta aastaarv: ");
+        int aastaarv = Integer.parseInt(scanner.nextLine());
 
-        if(vanus > 0 && vanus < 120) {
-            System.out.println("Hea teada.");
+        if(aastaarv % 4 == 0){
+            if(aastaarv % 100 == 0) {
+                if(aastaarv % 400 == 0) {
+                    System.out.println("Tegu on liigaastaga.");
+                } else {
+                    System.out.println("Tegu pole liigaastaga.");
+                }
+            }
+            else {
+                System.out.println("Tegu on liigaastaga.");
+            }
         }
         else {
-            System.out.println("Võimatu!");
+            System.out.println("Tegu pole liigaastaga.");
         }
     }
 }

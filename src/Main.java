c5d5d9b6066c7b4f2757300b247d1f5d6b5108eb
Andre Eli <andre.eli@khk.ee>
2020-10-04 21:16:24 +0300

    Ülesanne 10.

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner reader = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Sisesta esimene arv: ");
-        int esimene = Integer.parseInt(reader.nextLine());
-        System.out.print("Sisesta teine arv: ");
-        int teine = Integer.parseInt(reader.nextLine());
+        System.out.print("Sisesta ringi raadius: ");
+        int raadius = Integer.parseInt(scanner.nextLine());
 
-        double jagatis = (double)esimene / teine;
-
-        System.out.println("Jagamine: " + esimene + " / " + teine + " = " + jagatis);
+        System.out.println("Ringi ümbermõõt on " + 2 * Math.PI * raadius);
     }
 }

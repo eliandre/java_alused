4f1af14f98910d953d591980a60528caee7b4c89
Andre Eli <andre.eli@khk.ee>
2020-10-07 22:27:33 +0300

    Ülesanne 56.

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,23 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Sisesta sõna: ");
+        System.out.print("Sisesta tekst: ");
         String sisend = scanner.nextLine();
-        System.out.print("Sisesta sõnaosa pikkus: ");
-        int pikkus = Integer.parseInt(scanner.nextLine());
+        System.out.println("Ümber pööratult: " + reverse(sisend));
+    }
 
-        System.out.println("Tulemus: " + sisend.substring(0, pikkus));
+    public static String reverse(String tekst) {
+        String abi = "";
+        int x = 0;
+        int pikkus = tekst.length();
+        int algnePikkus = tekst.length();
+
+        while (x < algnePikkus) {
+            char taht = tekst.charAt(pikkus - 1);
+            abi += taht;
+            x++;
+            pikkus--;
+        }
+        return abi;
     }
 }

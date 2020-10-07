c18e945842248d9c9d38da90faf596f567daa2b9
Andre Eli <andre.eli@khk.ee>
2020-10-07 21:33:32 +0300

    Ülesanne 50.

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,17 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        String parool = "eesel";
 
-        while(true) {
-            System.out.print("Sisesta parool: ");
-            String sisend = scanner.nextLine();
-            if(sisend.equals(parool)){
-                System.out.println("Õige parool!");
-                System.out.println("Salasõna on yjdka sadd");
-                break;
-            }
+        System.out.print("Sisesta oma nimi: ");
+        String sisend = scanner.nextLine();
+
+        if (sisend.length() > 2) {
+            System.out.println("1. täht on " + sisend.charAt(0));
+            System.out.println("2. täht on " + sisend.charAt(1));
+            System.out.println("3. täht on " + sisend.charAt(2));
+        }
+        else {
+            System.out.println();
         }
     }
 }

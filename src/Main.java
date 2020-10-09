ad76c1aba0657adf58f9e66f4135dcff0713d07f
Andre Eli <andre.eli@khk.ee>
2020-10-09 16:13:58 +0300

    Ülesanne 61.

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,18 @@ import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-
         ArrayList<String> sonad = new ArrayList<String>();
+        sonad.add("Hallo");
+        sonad.add("Hello");
+        sonad.add("Ciao");
+        System.out.println("Nimekirjas on " + countItems(sonad) + " elementi.");
+    }
 
-        while(true) {
-            System.out.print("Sisesta sõna: ");
-            String sona = scanner.nextLine();
-            if(sona.isEmpty()){
-                break;
-            }
-            sonad.add(sona);
+    public static int countItems(ArrayList<String> list) {
+        int i = 0;
+        for (String items : list) {
+            i++;
         }
-
-        Collections.sort(sonad);
-
-        System.out.println("Sa sisestasid sõnad: ");
-        for(String sona : sonad){
-            System.out.println(sona);
-        }
+        return i;
     }
 }

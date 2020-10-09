478b6200d5f3d67cc835d277bbffc947fd982dbc
Andre Eli <andre.eli@khk.ee>
2020-10-09 15:04:34 +0300

    Ülesanne 57.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,25 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Sisesta tekst: ");
-        String sisend = scanner.nextLine();
-        System.out.println("Ümber pööratult: " + reverse(sisend));
-    }
+        ArrayList<String> sonad = new ArrayList<String>();
 
-    public static String reverse(String tekst) {
-        String abi = "";
-        int x = 0;
-        int pikkus = tekst.length();
-        int algnePikkus = tekst.length();
-
-        while (x < algnePikkus) {
-            char taht = tekst.charAt(pikkus - 1);
-            abi += taht;
-            x++;
-            pikkus--;
+        while(true) {
+            System.out.print("Sisesta sõna: ");
+            String sona = scanner.nextLine();
+            if(sona.isEmpty()){
+                break;
+            }
+            sonad.add(sona);
         }
-        return abi;
+
+        System.out.println("Sa sisestasid sõnad: ");
+        for(String sona : sonad){
+            System.out.println(sona);
+        }
     }
 }

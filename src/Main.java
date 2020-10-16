87f14e0eecc5047ede9b709fba891dad6b302e84
Andre Eli <andre.eli@khk.ee>
2020-10-16 13:21:48 +0300

    Ülesanne 77.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,44 @@
 public class Main {
     public static void main(String[] args) {
 
-        Menu menu = new Menu();
+        LyyraCard card = new LyyraCard(50);
 
-        menu.addMeal("Hamburger");
-        menu.addMeal("Pitsa");
-        menu.addMeal("Supp");
-        menu.addMeal("Hamburger");
+        System.out.println(card);
 
-        menu.printMeals();
+        card.payEconomical();
+        System.out.println(card);
 
-        menu.clearMenu();
-        menu.printMeals();
+        card.payGourmet();
+        card.payEconomical();
+        System.out.println(card);
 
+        card.loadMoney(15);
+        System.out.println(card);
+
+        card.loadMoney(20);
+        System.out.println(card);
+
+        card.loadMoney(-15);
+        System.out.println("Andre: " + card);
+
+        LyyraCard cardPekka = new LyyraCard(20);
+        LyyraCard cardBrian = new LyyraCard(30);
+
+        cardPekka.payGourmet();
+        cardBrian.payEconomical();
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
+
+        cardPekka.loadMoney(20);
+        cardBrian.payGourmet();
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
+
+        cardPekka.payEconomical();
+        cardPekka.payEconomical();
+
+        cardBrian.loadMoney(50);
+        System.out.println("Pekka: " + cardPekka);
+        System.out.println("Brian: " + cardBrian);
     }
 }

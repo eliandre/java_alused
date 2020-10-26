8782ff35540aed88d3b1bccaac263b2198c2c5a9
Andre Eli <andre.eli@khk.ee>
2020-10-26 21:39:39 +0200

    Ülesanne 100.1 ja 100.2 - Night sky.

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,11 @@
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
 
-        int[] original = {1, 2, 3, 4};
-        int[] copied = copy(original);
-        int[] reverse = reverseCopy(original);
+        NightSky NightSky = new NightSky(0.1, 40, 10);
+        NightSky NightSky2 = new NightSky(8, 4);
 
-        copied[0] = 99;
-
-        System.out.println("Originaal: " + Arrays.toString(original));
-        System.out.println("Kopeeritud: " + Arrays.toString(copied));
-        System.out.println("Tagurpidi: " + Arrays.toString(reverse));
-    }
-
-    public static int[] copy(int[] array) {
-        int[] uus = new int[array.length];
-        for(int i = 0; i < uus.length; i++) {
-            uus[i] = array[i];
-        }
-        return uus;
-    }
-
-    public static int[] reverseCopy(int[] array) {
-        int[] uus = new int[array.length];
-        int j = 0;
-        for(int i = array.length; i > 0; i--, j++) {
-            uus[j] = array[i-1];
-        }
-        return uus;
+        NightSky.printLine();
+        NightSky2.print();
     }
 }
---
+++ src/NightSky.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+
+public class NightSky {
+
+    private double density;
+    private double width;
+    private double height;
+
+    public NightSky(double density) {
+        this.density = density;
+        this.width = 20;
+        this.height = 10;
+    }
+
+    public NightSky(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.density = 0.1;
+    }
+
+    public NightSky(double density, int width, int height) {
+        this.density = density;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void printLine() {
+        Random rand = new Random();
+        for(int i = 0; i < this.width; i++) {
+            if(rand.nextDouble() <= this.density) {
+                System.out.print("*");
+            }
+            else System.out.print(" ");
+        }
+        System.out.println();
+    }
+
+    public void print() {
+        for(int i = 0; i < this.height; i++) {
+            this.printLine();
+        }
+    }
+}

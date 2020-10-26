b54fde881b974ce38c9dcbeb02162f5b5d74862b
Andre Eli <andre.eli@khk.ee>
2020-10-26 19:57:16 +0200

    Ülesanne 98 - Reversing and copying of an array.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = {5, 1, 3, 4, 2};
-        printElegantly(array);
+        int[] original = {1, 2, 3, 4};
+        int[] copied = copy(original);
+        int[] reverse = reverseCopy(original);
+
+        copied[0] = 99;
+
+        System.out.println("Originaal: " + Arrays.toString(original));
+        System.out.println("Kopeeritud: " + Arrays.toString(copied));
+        System.out.println("Tagurpidi: " + Arrays.toString(reverse));
     }
 
-    public static void printElegantly(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            if(i < array.length - 1) {
-                System.out.print(array[i] + ", ");
-            }
-            else System.out.print(array[i] + "");
+    public static int[] copy(int[] array) {
+        int[] uus = new int[array.length];
+        for(int i = 0; i < uus.length; i++) {
+            uus[i] = array[i];
         }
+        return uus;
+    }
+
+    public static int[] reverseCopy(int[] array) {
+        int[] uus = new int[array.length];
+        int j = 0;
+        for(int i = array.length; i > 0; i--, j++) {
+            uus[j] = array[i-1];
+        }
+        return uus;
     }
 }

5e17b1f73210a1068aa1af500d97c8cdcd6c9ff7
Andre Eli <andre.eli@khk.ee>
2020-10-26 16:44:19 +0200

    Ülesanne 93.1 - Person extended.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        Clock clock = new Clock(23, 59, 50);
+        Person pekka = new Person("Pekka", 15, 2, 1993);
+        Person steve = new Person("Thomas", 1,3, 1955);
 
-        int i = 0;
-        while (i < 20) {
-            System.out.println(clock);
-            clock.tick();
-            i++;
-        }
+        System.out.println(steve.getName() + " vanus on " + steve.age() + " aastat.");
+        System.out.println(pekka.getName() + " vanus on " + pekka.age() + " aastat.");
 
     }
 }

7e47e547b200f4cd4902460dfe0569e20224fba6
Andre Eli <andre.eli@khk.ee>
2020-11-19 10:15:17 +0200

    Ülesanne 11.1 ja 11.2 - Boxes and Things (Main file changed).

--- src/Main.java
+++ src/Main.java
@@ -19,13 +19,13 @@ public class Main {
 
         Box box = new Box(10);
 
-        box.add( new Raamat("Fedor Dostojevski", "Crime and Punishment", 2) ) ;
-        box.add( new Raamat("Robert Martin", "Clean Code", 1) );
-        box.add( new Raamat("Kent Beck", "Test Driven Development", 0.7) );
+        box.add(raamat1);
+        box.add(raamat2);
+        box.add(raamat3);
 
-        box.add( new CD("Pink Floyd", "Dark Side of the Moon", 1973) );
-        box.add( new CD("Wigwam", "Nuclear Nightclub", 1975) );
-        box.add( new CD("Rendezvous Park", "Closer to Being Here", 2012) );
+        box.add(cd1);
+        box.add(cd2);
+        box.add(cd3);
 
         System.out.println(box);
     }

4a9c4bd707a0c0f770cf8644bc1a8a4eb2cc781b
Andre Eli <andre.eli@khk.ee>
2020-11-19 12:15:47 +0200

    Ülesanne 12.1 ja 12.2 - Online Shop.

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,29 @@
 public class Main {
     public static void main(String[] args) {
 
-        Raamat raamat1 = new Raamat("Fedor Dostojevski", "Crime and Punishment", 2);
-        Raamat raamat2 = new Raamat("Robert Martin", "Clean Code", 1);
-        Raamat raamat3 = new Raamat("Kent Beck", "Test Driven Development", 0.5);
+        Storehouse store = new Storehouse();
 
-        CD cd1 = new CD("Pink Floyd", "Dark Side of the Moon", 1973);
-        CD cd2 = new CD("Wigwam", "Nuclear Nightclub", 1975);
-        CD cd3 = new CD("Rendezvous Park", "Closer to Being Here", 2012);
+        store.addProduct("pett", 3, 10);
+        store.addProduct("viin", 5, 7);
 
-        System.out.println(raamat1);
-        System.out.println(raamat2);
-        System.out.println(raamat3);
-        System.out.println(cd1);
-        System.out.println(cd2);
-        System.out.println(cd3);
+        System.out.println("Hinnad: ");
+        System.out.println("Pett: " + store.price("pett"));
+        System.out.println("Viin: " + store.price("viin"));
+        System.out.println("Suhkur: " + store.price("suhkur"));
 
-        Box box = new Box(10);
+        Storehouse store1 = new Storehouse();
+        store1.addProduct("kohv", 5, 1);
 
-        box.add(raamat1);
-        box.add(raamat2);
-        box.add(raamat3);
+        System.out.println("Laos: ");
+        System.out.println("Kohv: " + store1.stock("kohv"));
+        System.out.println("Suhkur: " + store1.stock("suhkur"));
 
-        box.add(cd1);
-        box.add(cd2);
-        box.add(cd3);
+        System.out.println("Võtame kohvi: " + store1.take("kohv"));
+        System.out.println("Võtame kohvi: " + store1.take("kohv"));
+        System.out.println("Võtame suhkrut: " + store1.take("suhkur"));
 
-        System.out.println(box);
-
-        Box box1 = new Box(20);
-        box1.add(new Raamat("Oskar Luts", "Kevade", 2));
-        box1.add(new Raamat("Lev Tolstoi", "Sõda ja rahu", 7));
-        System.out.println(box1);
-        box1.add(box);
-        System.out.println(box1);
+        System.out.println("Laos:");
+        System.out.println("Kohv: " + store1.stock("kohv"));
+        System.out.println("Suhkur: " + store1.stock("suhkur"));
     }
 }
---
+++ src/Storehouse.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Storehouse {
+
+    private Map<String, Integer> toodeteHinnad;
+    private Map<String, Integer> toodeteKogus;
+
+    public Storehouse() {
+        toodeteHinnad = new HashMap<>();
+        toodeteKogus = new HashMap<>();
+    }
+
+    public void addProduct(String product, int price, int stock) {
+        toodeteHinnad.put(product,price);
+        toodeteKogus.put(product,stock);
+    }
+
+    public int price(String product) {
+        if(toodeteHinnad.containsKey(product)) {
+            return toodeteHinnad.get(product);
+        }
+        else return -99;
+    }
+
+    public int stock(String product) {
+        if(toodeteKogus.containsKey(product)) {
+            return toodeteKogus.get(product);
+        }
+        return 0;
+    }
+
+    public boolean take(String product) {
+        if(toodeteKogus.containsKey(product) && toodeteKogus.get(product) > 0) {
+            toodeteKogus.put(product, toodeteKogus.get(product) - 1);
+            return true;
+        }
+        return false;
+    }
+}

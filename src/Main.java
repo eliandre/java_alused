addbcef1d12639c9b1ed2efae5d57e73f8b08c63
Andre Eli <andre.eli@khk.ee>
2020-11-19 13:44:43 +0200

    Ülesanne 12.3 - Online Shop.

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,14 @@ public class Main {
 
         Storehouse store = new Storehouse();
 
-        store.addProduct("pett", 3, 10);
-        store.addProduct("viin", 5, 7);
+        store.addProduct("piim", 3, 10);
+        store.addProduct("kohv", 5, 6);
+        store.addProduct("pett", 2, 20);
+        store.addProduct("jogurt", 2, 30);
 
-        System.out.println("Hinnad: ");
-        System.out.println("Pett: " + store.price("pett"));
-        System.out.println("Viin: " + store.price("viin"));
-        System.out.println("Suhkur: " + store.price("suhkur"));
-
-        Storehouse store1 = new Storehouse();
-        store1.addProduct("kohv", 5, 1);
-
-        System.out.println("Laos: ");
-        System.out.println("Kohv: " + store1.stock("kohv"));
-        System.out.println("Suhkur: " + store1.stock("suhkur"));
-
-        System.out.println("Võtame kohvi: " + store1.take("kohv"));
-        System.out.println("Võtame kohvi: " + store1.take("kohv"));
-        System.out.println("Võtame suhkrut: " + store1.take("suhkur"));
-
-        System.out.println("Laos:");
-        System.out.println("Kohv: " + store1.stock("kohv"));
-        System.out.println("Suhkur: " + store1.stock("suhkur"));
+        System.out.println("Tooted:");
+        for(String product : store.products()) {
+            System.out.println(product);
+        }
     }
 }
--- src/Storehouse.java
+++ src/Storehouse.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Storehouse {
 
@@ -36,5 +37,9 @@ public class Storehouse {
             return true;
         }
         return false;
+    }
+
+    public Set<String> products() {
+        return toodeteKogus.keySet();
     }
 }

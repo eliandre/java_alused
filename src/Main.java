cd93ae10408a782225dc026b6af45f6b0db24615
Andre Eli <andre.eli@khk.ee>
2020-11-22 16:44:26 +0200

    Ülesanne 15.5 - Sorting Cards.

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
 
-        Hand hand1 = new Hand();
-        hand1.add(new Card(2, Card.SPADES));
-        hand1.add(new Card(14, Card.CLUBS));
-        hand1.add(new Card(12, Card.HEARTS));
-        hand1.add(new Card(2, Card.CLUBS));
+        ArrayList<Card> cards = new ArrayList<Card>();
 
-        Hand hand2 = new Hand();
-        hand2.add(new Card(11, Card.DIAMONDS));
-        hand2.add(new Card(11, Card.CLUBS));
-        hand2.add(new Card(11, Card.HEARTS));
+        cards.add(new Card(3, Card.CLUBS));
+        cards.add(new Card(2, Card.DIAMONDS));
+        cards.add(new Card(14, Card.CLUBS));
+        cards.add(new Card(12, Card.HEARTS));
+        cards.add(new Card(2, Card.CLUBS));
 
-        int comparsion = hand1.compareTo(hand2);
+        SortAgainstSuitAndValue suitSorter = new SortAgainstSuitAndValue();
+        Collections.sort(cards, suitSorter);
 
-        if(comparsion < 0) {
-            System.out.println("Väärtuslikeim käsi sisaldab kaarte:");
-            hand2.print();
+        for(Card c : cards) {
+            System.out.println(c);
         }
-        else if (comparsion > 0) {
-            System.out.println("Väärtuslikeim käsi sisaldab kaarte:");
-            hand1.print();
-        }
-        else System.out.println("Käed on sama väärtusega.");
     }
 }

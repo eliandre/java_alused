47048b7c010bb577526d7d203b687c4ef232f078
Andre Eli <andre.eli@khk.ee>
2020-11-23 14:28:30 +0200

    Ülesanne 18.1 - Moving.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,10 @@
-import java.util.ArrayList;
-import java.util.Collections;
+import moving.domain.Item;
+import moving.domain.Thing;
 
 public class Main {
     public static void main(String[] args) {
 
-        Hand hand = new Hand();
-
-        hand.add( new Card(12, Card.HEARTS) );
-        hand.add( new Card(4, Card.CLUBS) );
-        hand.add( new Card(2, Card.DIAMONDS) );
-        hand.add( new Card(14, Card.CLUBS) );
-        hand.add( new Card(7, Card.HEARTS) );
-        hand.add( new Card(2, Card.CLUBS) );
-
-        hand.sortAgainstSuit();
-        hand.print();
+        Thing item = new Item("hambahari", 2);
+        System.out.println(item);
     }
 }
